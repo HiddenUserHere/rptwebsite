8f7ff721fcaefb2751f1e9fd944144f01b8a021b
Nivaldo Carvalho <nivaldo_j@outlook.com>
2023-04-01 17:00:22 -0300

    Moved SocialLinks to its component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar/index.jsx';
 import Footer from './components/Footer/index.jsx';
-import SocialLinks from './components/SocialLinks';
+import SocialLinks from './components/SocialLinks/index.jsx';
 import { schema } from './components/Navbar/settings.jsx';
 
 
