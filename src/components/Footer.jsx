fdb5a24de2cd1ba6aadee3c1b88e4a53a9fe8930
Nivaldo Carvalho <nivaldo_j@outlook.com>
2023-04-01 10:00:13 -0300

    Removed footer buttons (home, media, etc)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,37 +8,34 @@ const Footer = () => {
   return (
     <footer>
       <nav>
-        <div className="footersocial2">
+        <div className="footer-social-2">
           <ul className="social-icons">
-            <li><Link to={DISCORD_URL}><FaDiscord/></Link></li>
-            <li><Link to={YOUTUBE_URL}><FaYoutube/></Link></li>
-            <li><Link to={FACEBOOK_URL}><FaFacebook/></Link></li>
-            <li><Link to={INSTAGRAM_URL}><FaInstagram/></Link></li>
-          </ul>
-        </div>
-        <div className="footerbuttons">
-          <ul>
-            <li><Link className="fontstyle">Home</Link></li>
-            <li><Link className="fontstyle">Gameplay</Link></li>
-            <li><Link className="fontstyle">Media</Link></li>
-            <li><Link className="fontstyle">Community</Link></li>
-            <li><Link className="fontstyle">Forums</Link></li>
-          </ul>
-        </div>
-        <div className="footersocial">
-          <ul>
-            <li><Link className="fontstyle">Support</Link></li>
-            <li><Link className="fontstyle">Terms</Link></li>
-            <li><Link className="fontstyle">Privacy</Link></li>
-            <li><Link className="fontstyle">About</Link></li>
+            <li>
+              <Link to={ DISCORD_URL }>
+                <FaDiscord title="Discord"/>
+              </Link>
+            </li>
+            <li>
+              <Link to={ YOUTUBE_URL }>
+                <FaYoutube title="YouTube"/>
+              </Link>
+            </li>
+            <li>
+              <Link to={ FACEBOOK_URL }>
+                <FaFacebook title="Facebook"/>
+              </Link>
+            </li>
+            <li>
+              <Link to={ INSTAGRAM_URL }>
+                <FaInstagram title="Instagram"/>
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="haru">
-          <a href="https://wa.me/5524992024737" target={'_blank'}><h1>HARU</h1>
-            <h2>DESIGN</h2></a>
-
+          <h1>HARU</h1>
+          <h2>DESIGN</h2>
         </div>
-
       </nav>
     </footer>
   );
